9258582b99488591556caace6498115ebf031a22
Servbolt Systems <servboltsystems@gmail.com>
2013-12-10 13:17:46 +0530

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetasaserviceCom::Application.config.secret_key_base = 'f7752d0d8273e9f46d8f027a22f79c1d27ead552de412c0c2fca853a01a696c1f79152bd11df27e6a3b6eed1434efcc2930a22c91f10220f62c93965c48bbcee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlanetasaserviceCom::Application.config.session_store :cookie_store, key: '_planetasaservice_com_session'
